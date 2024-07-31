4c9ecada0af519ec298f5211c3d84be1f6534dab
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-07-31 12:25:38 +0530

    Add New Project PDF-X

---
+++ PDF Merger Website/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+  merger.add('pdf2.pdf', [1, 3]); // merge the pages 1 and 3
+//   merger.add('pdf2.pdf', '4, 7, 8'); // merge the pages 4, 7 and 8
+//   merger.add('pdf3.pdf', '1 to 2'); //merge pages 1 to 2
+//   merger.add('pdf3.pdf', '3-4'); //merge pages 3 to 4
