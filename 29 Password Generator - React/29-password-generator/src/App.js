80aa489ce9c568887edfc9e8ea22e56d64313453
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-10-20 18:35:39 +0530

    Code

--- 29 Password Generator - React/29-password-generator/src/App.js
+++ 29 Password Generator - React/29-password-generator/src/App.js
@@ -1,10 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="passwordBox">
+      <h2>Password Genrater</h2>
+
+      <div className="passwordBoxIn">
+        <input type="text" readOnly /> <button>Copy</button>
+      </div>
+
+      <div className="passwordLength">
+        <label>Password Length</label>
+        <input type="number" max={20} min={1} />
+      </div>
+
+      <div className="passwordLength">
+        <label>Included Uppercase Letters</label>
+        <input type="checkbox" />
+      </div>
+
+      <div className="passwordLength">
+        <label>Included Lowercase Letters</label>
+        <input type="checkbox" />
+      </div>
+
+      <div className="passwordLength">
+        <label>Included Numbers</label>
+        <input type="checkbox" />
+      </div>
+
+      <div className="passwordLength">
+        <label>Included Symbols</label>
+        <input type="checkbox" />
+      </div>
+
+      <div>
+        <button className="btn">Generate Password</button>
+      </div>
     </div>
   );
 }
