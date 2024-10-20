2cc97bdbbb368136b535495019b2371b6e3e660d
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-10-20 14:32:52 +0530

    Code

--- 30 Create News App - React Bootstrap/news-app-vite/src/components/NewsItems.jsx
+++ 30 Create News App - React Bootstrap/news-app-vite/src/components/NewsItems.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const NewsItems = () => {
+const NewsItems = ({ title, description, src, url }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+   
+      <div className="card bg-dark text-light d-inline-block my-3 mx-3 px-2 py-2 " style={{ width: "400px", height: "500px"}}>
+        <img src={src} style={{height: "200px"}} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description ? description: "Information technology companies have posted strong results for the second quarter of FY25, except Tata Consultancy Services (TCS)"}</p>
+          <a href={url} className="btn btn-primary">
+            Read More
+          </a>
+        </div>
+      </div>
 
-export default NewsItems
+  );
+};
+
+export default NewsItems;
