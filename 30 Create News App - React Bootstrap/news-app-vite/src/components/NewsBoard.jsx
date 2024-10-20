6392dbc502f86b086d4da5ac17c70598d3fe879f
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-10-20 10:58:42 +0530

    Code

---
+++ 30 Create News App - React Bootstrap/news-app-vite/src/components/NewsBoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsBoard = () => {
+  return (
+    <div>
+      <h1 className='text-center'>Latest <span className=''>News</span></h1>
+    </div>
+  )
+}
+
+export default NewsBoard
---
+++ 30 Create News App - React Bootstrap/news-app-vite/src/components/NewsItems.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsItems = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NewsItems
