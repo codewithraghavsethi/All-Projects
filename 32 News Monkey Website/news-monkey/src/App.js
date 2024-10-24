da61442e2fe2302417bd96352620fa7936fe0c82
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-10-24 20:06:37 +0530

    Code

--- 32 News Monkey Website/news-monkey/src/App.js
+++ 32 News Monkey Website/news-monkey/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NavbarReact from './components/NavbarReact'
 import News from './components/News'
 
+
 export default class App extends Component {
   render() {
     return (
