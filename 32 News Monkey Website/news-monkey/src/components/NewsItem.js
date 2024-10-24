e55094ed0a52d72b9a1cf0cfa3df6033ec0c461b
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-10-24 09:25:39 +0530

    Code

--- 32 News Monkey Website/news-monkey/src/components/NewsItem.js
+++ 32 News Monkey Website/news-monkey/src/components/NewsItem.js
@@ -9,8 +9,9 @@ export default class NewsItem extends Component {
     return (
       <div className="container my-3">
         <Card style={{ width: "18rem", height: "420px"}}  >
-          <Card.Img variant="top" src={imageUrl} style={{height: "250px"}} />
+          <Card.Img variant="top" src={imageUrl ? imageUrl : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYN09MsHDipPCV4-D-ECTD1y-HLUOA9eSy_g&s"} style={{ height: "200px" }} />
           <Card.Body>
+         
             <Card.Title>{title}.....</Card.Title>
             <Card.Text>{description}.....</Card.Text>
             <Button variant="primary" href={newsUrl} target="_blank">Read More</Button>
